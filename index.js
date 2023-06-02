4c2c043920661adc384d6d13e24ddeb7b4c0b8ad
Tyler Aycock <tyler.joseph.aycock@gmail.com>
2023-06-02 06:20:32 -0700

    finished intermediate portion

--- index.js
+++ index.js
@@ -1,61 +1,44 @@
-
+const inputField = document.querySelector(`input`)
+const list = document.querySelector(`ul`)
+const theForm = document.querySelector(`form`)
 const message = document.querySelector(`#message`)
 
 const addMovie = (event) => {
-
-let inputField = document.querySelector('input')
-
-const movie = document.createElement(`li`)
-
-const movieTitle = document.createElement(`span`)
-
-  event.preventDefault()
-
-  let formElement = document.querySelector('ul')
-
-  // stores input field as value saved to movietitle
-  movieTitle.textContent = inputField.value
-  
-  movieTitle.addEventListener(`click`, crossOffMovie)
-
-  // appends movieTitle to Movie object
-  movie.appendChild(movieTitle)
-
-  const deleteBtn = document.createElement('button')
-
-  deleteBtn.textContent = 'X';
-  
-  deleteBtn.addEventListener('click', deleteMovie)
-
-  movie.appendChild(deleteBtn)
-
-  // appends movie obj to ul
-  formElement.appendChild(movie)
-
-  inputField.value = ''
-  
+    event.preventDefault()
+    const movie = document.createElement('li')
+    const movieTitle = document.createElement('span')
+    movieTitle.textContent = inputField.value 
+    movieTitle.addEventListener(`click`, crossOffMovie)
+    movie.appendChild(movieTitle)
+    const deleteBtn = document.createElement(`button`)
+    deleteBtn.textContent =`X`
+    deleteBtn.addEventListener('click', deleteMovie)
+    movie.appendChild(deleteBtn)
+    list.appendChild(movie)
+    inputField.value = ` `
 }
+
+theForm.addEventListener(`submit`, addMovie)
 
 const deleteMovie = (event) => {
     event.target.parentNode.remove()
-    message.textContent = `Movie deleted!`
+    message.textContent = `${event.target.nextElementSibling} deleted`
+    revealMessage()
 }
 
-
-const selectForm = document.querySelector('form')
-
-selectForm.addEventListener('submit', event => {
-
-  addMovie(event)
-
-})
-
-
-const crossOffMovie = event => {
+const crossOffMovie = (event) => {
     event.target.classList.toggle(`checked`)
     if (event.target.classList.contains(`checked`)){
-        message.textContent = `Movie Watched`
-    } else {
-        message.textContent = `Movie added back`
+        message.textContent = `${event.target.textContent} watched`
+    } else{
+        message.textContent = `${event.target.textContent} unwatched`
     }
+    revealMessage()
+}
+
+const revealMessage = ()=>{
+    message.classList.remove(`hide`)
+    setTimeout(()=>{
+        message.classList.add(`hide`)
+    }, 2000)
 }
